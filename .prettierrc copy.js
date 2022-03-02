3490317592f95a41338bc8051e549ae52059d8d6
Писарев Кирилл <k.pisarev@maximatelecom.ru>
2022-03-02 13:32:40 +0300

    chore: add linters

---
+++ .prettierrc copy.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: 'typescript',
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  proseWrap: 'never',
+};
